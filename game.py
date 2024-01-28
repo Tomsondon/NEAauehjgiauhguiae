5de33452f1a4b45fae253c7af56dd7ae0fb2b1e8
Tomsondon <125699372+Tomsondon@users.noreply.github.com>
2024-01-28 14:55:32 +0000

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,11 @@
 import os
+from tkinter import messagebox
+
 import boards
 import pygame
 import datetime
+import database
+import tkinter
 
 f = open("config.txt")
 config = f.read()
@@ -48,17 +52,13 @@ def Square(x, y, size):
     return pygame.Rect(x - size / 2, y - size / 2, size, size)
 
 
-class Game:
-    def __init__(self, lives, Level, maze):
-        self._board = maze
-        self._time = pygame.time.get_ticks()
+class Board:
+    def __init__(self, boardName):
+        self._board = boardName
         self.__dotsLeft = 0
-        self.__score = 0
-        self.__lives = lives
         self.__wallPositions = []
         self.__pelletPositions = []
         self.__ogPelletPositions = []
-        self.__pelletPositions = []
         self.__junctionPositions = []
 
         for j in range(len(self._board)):
@@ -78,18 +78,6 @@ class Game:
         for junctions in boards.mazeMapping(self._board):
             pos = getCoord(junctions[1], junctions[0])
             self.__junctionPositions.append(Square(pos[0], pos[1], 2))
-
-    def addLives(self, lives):
-        self.__lives += lives
-
-    def getLives(self):
-        return self.__lives
-
-    def getScore(self):
-        return self.__score
-
-    def addScore(self, score):
-        self.__score += score
 
     def getBoard(self):
         return self._board
@@ -113,12 +101,16 @@ class Game:
                     return 1
         return 0
 
-    def Render(self, screen):
-        self._time = pygame.time.get_ticks()
-        drawValue("Score", self.__score, (0, 800), screen)
-        drawValue("Time", self._time / 1000, (300, 800), screen)
-        drawValue("Lives", self.__lives, (600, 800), screen)
-        drawValue("Dots left", self.getDotsLeft(), (0, 900), screen)
+    def getDotsLeft(self):
+        return len(self.__pelletPositions)
+
+    def coordInJunction(self, xcoord, ycoord):
+        for junctions in self.getJunctionPositions():
+            if junctions.collidepoint(xcoord, ycoord):
+                return True
+        return False
+
+    def render(self, screen):
         for i in self.__wallPositions:
             pygame.draw.rect(screen, "blue", i, 1)
         for i in self.__pelletPositions:
@@ -126,27 +118,74 @@ class Game:
         for i in self.__junctionPositions:
             pygame.draw.rect(screen, "yellow", i, 3)
 
-    def getDotsLeft(self):
-        return len(self.__pelletPositions)
+    def resetBoard(self):
+        for pellet in self.__ogPelletPositions:
+            self.__pelletPositions.append(pellet)
+
+    def getCoord(self, x, y):
+        coordX = (24 * x) + 12
+        coordY = (24 * y) + 12
+        return coordX, coordY
+
+    def getGridRef(self, x, y):
+        refX = x // 24
+        refY = y // 24
+        return int(refX), int(refY)
+
+
+class Game:
+    def __init__(self, lives, Level, board, ghosts, pacman):
+        self._time = 0
+        self.__originalTime = pygame.time.get_ticks()
+        self.__score = 0
+        self.__lives = lives
+        self.__level = Level
+        self._board = board
+        self._ghosts = ghosts
+        self._pacman = pacman
+
+    def addLives(self, lives):
+        self.__lives += lives
+
+    def getLives(self):
+        return self.__lives
+
+    def getScore(self):
+        return self.__score
+
+    def addScore(self, score):
+        self.__score += score
+
+    def getLevel(self):
+        return self.__level
+
+    def loadNextLevel(self):
+        self.__level += 1
+        self._board.Reset()
+        self._ghosts.resetGhosts()
+        self._pacman.reset()
+
+    def render(self, screen):
+        self._time = pygame.time.get_ticks() - self.__originalTime
+        drawValue("Score", self.__score, (0, 800), screen)
+        drawValue("Time", self._time / 1000, (300, 800), screen)
+        drawValue("Lives", self.__lives, (600, 800), screen)
+        drawValue("Level", self.__level, (600, 900), screen)
 
     def getTime(self):
         return self._time
 
-    def inJunction(self, xcoord, ycoord):
-        for junctions in self.getJunctionPositions():
-            if junctions.collidepoint(xcoord, ycoord):
-                return True
-        return False
-
 
 class Entity:
-    def __init__(self, img, position):
+    def __init__(self, img, position, isPlayer):
         self._imgJPG = img
         self._img = pygame.transform.scale(pygame.image.load(img), (24, 24))
+        self._startPos = position
         self._position = pygame.Vector2(position)
         self._direction = 0
         self._speed = 1
         self._boundBox = Square(self._position.x, self._position.y, 24)
+        self._isPlayer = isPlayer
 
     def getBoundBox(self):
         return self._boundBox
@@ -171,14 +210,14 @@ class Entity:
     def getSpeed(self):
         return self._speed
 
-    def Render(self, screen, dt):
-        self.UpdatePos(dt)
+    def render(self, screen, dt):
+        self.updatePos(dt)
         self._boundBox = Square(self._position.x, self._position.y, 24)
         screen.blit(self._img, (self._position.x - 12, self._position.y - 12))
         pygame.draw.rect(screen, "red", self._boundBox, 1)
         pygame.draw.circle(screen, "green", self._position, 3)
 
-    def UpdatePos(self, dt):  # Direction 1 is up, 2 is down, 3 is left, 4 is right
+    def updatePos(self, dt):  # Direction 1 is up, 2 is down, 3 is left, 4 is right
         if self._direction == 1:
             self._position.y -= self._speed * 300 * dt
         if self._direction == 2:
@@ -196,10 +235,10 @@ class Entity:
             self._position.x = 0
 
 
-class Player(Entity):
+class Pacman(Entity):
     def __init__(self):
         self.__startPoint = getCoord(14, 24)
-        Entity.__init__(self, "images/player.jpg", (self.__startPoint[0], self.__startPoint[1]))
+        Entity.__init__(self, "images/player.jpg", (self.__startPoint[0], self.__startPoint[1]), True)
         self.__nextDirection = 0
 
     def addDirection(self, newDirection, isFree):
@@ -212,7 +251,7 @@ class Player(Entity):
         print("Current Direction", self._direction)
         print("Next Direction", self.__nextDirection)
 
-    def Restart(self):
+    def restart(self):
         self._position = pygame.Vector2(self.__startPoint[0], self.__startPoint[1])
         self._direction = 0
         self.__nextDirection = 0
@@ -220,19 +259,27 @@ class Player(Entity):
 
 
 class Ghost(Entity):
-    def __init__(self, ghostType):  # 0 is blinky, 1 is inky, 2 is pinky, 3 is clyde
+    def __init__(self, ghostType, isPlayer):  # 0 is blinky, 1 is inky, 2 is pinky, 3 is clyde
         self._manUp = 9999999999  # timer for scared phase, determines at what tick the ghost should stop being scared
         if ghostType == 0:
-            Entity.__init__(self, "images/blinky.jpg", (336, 384))
+            Entity.__init__(self, "images/blinky.jpg", (336, 384), isPlayer)
         elif ghostType == 1:
-            Entity.__init__(self, "images/inky.jpg", (336, 360))
+            Entity.__init__(self, "images/inky.jpg", (336, 360), isPlayer)
         elif ghostType == 2:
-            Entity.__init__(self, "images/pinky.jpg", (312, 384))
+            Entity.__init__(self, "images/pinky.jpg", (312, 384), isPlayer)
         elif ghostType == 3:
-            Entity.__init__(self, "images/clyde.jpg", (312, 360))
+            Entity.__init__(self, "images/clyde.jpg", (312, 360), isPlayer)
         self._isScared = False
         self._isDead = False
         self._chaseMode = 1  # 0 is scatter state, 1 is chase state
+
+    def reset(self):  # resets the ghost
+        self._position = pygame.Vector2(self._startPos)
+        self._speed = 1
+        self._manUp = 9999999999
+        self._isScared = False
+        self._isDead = False
+        self._chaseMode = 1
 
     def scareGhost(self):
         self._manUp = pygame.time.get_ticks() + 5000
@@ -263,8 +310,8 @@ class Ghost(Entity):
         self._isDead = True
         self._isScared = False
 
-    def Update(self, dt):
-        super().Update(dt)
+    def update(self, dt):
+        super().update(dt)
         if pygame.time.get_ticks() >= self._manUp:
             self._isScared = False
             self._img = pygame.transform.scale(pygame.image.load(self._imgJPG), (24, 24))
@@ -274,10 +321,13 @@ class Ghost(Entity):
     def isScared(self):
         return self._isScared
 
+    def isDead(self):
+        return self._isDead
+
 
 class Blinky(Ghost):
-    def __init__(self):
-        Ghost.__init__(self, 0)
+    def __init__(self, isPlayer):
+        Ghost.__init__(self, 0, isPlayer)
 
     def chasePlayer(self, playerPos):
         displacementVector = list(playerPos - self.getPosition())
@@ -294,8 +344,8 @@ class Blinky(Ghost):
 
 
 class Inky(Ghost):
-    def __init__(self):
-        Ghost.__init__(self, 1)
+    def __init__(self, isPlayer):
+        Ghost.__init__(self, 1, isPlayer)
 
     def chasePlayer(self, playerPos, playerDir, blinkyPos):
         posArr = list(playerPos)
@@ -319,8 +369,8 @@ class Inky(Ghost):
 
 
 class Pinky(Ghost):
-    def __init__(self):
-        Ghost.__init__(self, 2)
+    def __init__(self, isPlayer):
+        Ghost.__init__(self, 2, isPlayer)
 
     def chasePlayer(self, playerPos, playerDir):
         targetVector = list(playerPos)
@@ -345,6 +395,64 @@ class Pinky(Ghost):
                 self._direction = 2
 
 
+class GhostGroup:
+    def __init__(self, *ghosts):
+        self.__ghosts = []
+        for entity in ghosts:
+            self.__ghosts.append(entity)
+
+    def ghostCollision(self, boundBox, screen):
+        for ghost in self.__ghosts:
+            if boundBox.colliderect(ghost.getBoundBox()):
+                return True
+
+    def resetGhosts(self):
+        for ghost in self.__ghosts:
+            ghost.reset()
+
+    def scareGhosts(self):
+        for ghost in self.__ghosts:
+            if not (ghost.isDead()):
+                ghost.scareGhost()
+
+class Referee(Board):
+
+    def isNextBlockWall(self, direction, gridPosition):
+        if direction == 1:
+            if self._board.getBoard()[gridPosition[1] - 1][gridPosition[0]] == 3:
+                return True
+            else:
+                return False
+
+        if direction == 2:
+            if self._board.getBoard()[gridPosition[1] + 1][gridPosition[0]] == 3:
+                return True
+            else:
+                return False
+
+        if direction == 3:
+            if self._board.getBoard()[gridPosition[1]][gridPosition[0] - 1] == 3:
+                return True
+            else:
+                return False
+
+        try:
+            if direction == 4:
+                if self._board.getBoard()[gridPosition[1]][gridPosition[0] + 1] == 3:
+                    return True
+                else:
+                    return False
+        except IndexError:  # Error handling for the case where the player enters a right side warp; in this case there wouldn't be an incoming collision
+            return False
+
+
+
+
+
+
+
+
+
 ########################################################MAIN PROGRAM####################################################
 def main():
     pygame.init()
@@ -360,45 +468,16 @@ def main():
     fps = 240
     dt = 0
     level = 1
-    game = Game(3, level, boards.boardsdict["default"])
-    hero = Player()
-    blinky = Blinky()
-    inky = Inky()
-    pinky = Pinky()
-    clyde = Ghost(3)
-
-    def ghostCollision():
-        if heroBox.colliderect(blinky.getBoundBox()) or heroBox.colliderect(inky.getBoundBox()) or heroBox.colliderect(
-                pinky.getBoundBox()) or heroBox.colliderect(clyde.getBoundBox()):
-            return True
-
-    def incomingCollision(direction):
-        if direction == 1:
-            if game.getBoard()[gridPosition[1] - 1][gridPosition[0]] == 3:
-                return True
-            else:
-                return False
-
-        if direction == 2:
-            if game.getBoard()[gridPosition[1] + 1][gridPosition[0]] == 3:
-                return True
-            else:
-                return False
-
-        if direction == 3:
-            if game.getBoard()[gridPosition[1]][gridPosition[0] - 1] == 3:
-                return True
-            else:
-                return False
-
-        if direction == 4:
-            if game.getBoard()[gridPosition[1]][gridPosition[0] + 1] == 3:
-                return True
-            else:
-                return False
-
-        else:
-            return False
+    selectedBoard = boards.boardsdict["default"]
+    board = Board(selectedBoard)
+    pacman = Pacman()
+    blinky = Blinky(False)
+    inky = Inky(False)
+    pinky = Pinky(False)
+    clyde = Ghost(3, False)
+    ghosts = GhostGroup(blinky, inky, pinky, clyde)
+    game = Game(3, level, board, ghosts, pacman)
+    referee = Referee(selectedBoard)
 
     while running:
         for event in pygame.event.get():
@@ -407,7 +486,7 @@ def main():
 
         screen.fill("black")
 
-        currentPos = hero.getPosition()
+        currentPos = pacman.getPosition()
         gridPosition = getGridRef(currentPos[0], currentPos[1])
 
         newDirection = 0
@@ -421,37 +500,29 @@ def main():
         elif keys[pygame.K_d]:
             newDirection = 4
 
-        if game.inJunction(currentPos.x, currentPos.y):
-            print("inside junction")
-            if incomingCollision(hero.getDirection()):
-                hero.addDirection(0, True)
 
-            if keys[pygame.K_w]:
-                hero.addDirection(1, not (incomingCollision(1)))
-            elif keys[pygame.K_s]:
-                hero.addDirection(2, not (incomingCollision(2)))
-            elif keys[pygame.K_a]:
-                hero.addDirection(3, not (incomingCollision(3)))
-            elif keys[pygame.K_d]:
-                hero.addDirection(4, not (incomingCollision(4)))
+
 
         if keys[pygame.K_ESCAPE]:
             running = False
-        if game.getDotsLeft() == 0:
+        if board.getDotsLeft() == 0:
             print("Level complete!")
-            running = False
+            game.restart()
+            pacman.restart()
+            ghosts.resetGhosts()
 
-        drawValue("Speed", hero.getSpeed(), (400, 900), screen)
+        drawValue("Speed", pacman.getSpeed(), (400, 900), screen)
         # if keys[pygame.K_t]:
-        # hero.addSpeed(-0.01)
+        # pacman.addSpeed(-0.01)
         #  if keys[pygame.K_y]:
-        #       hero.addSpeed(0.01)
+        #       pacman.addSpeed(0.01)
         #  if keys[pygame.K_p]:
-        #       print(hero.getPosition())
+        #       print(pacman.getPosition())
 
-        game.Render(screen)
-        hero.Render(screen, dt)
-        kys = hero.getDirection()
+        board.render(screen)
+        game.render(screen)
+        pacman.render(screen, dt)
+        kys = pacman.getDirection()
         if inky.isScared():
             blinky.runAway(currentPos)
             inky.runAway(currentPos)
@@ -461,31 +532,26 @@ def main():
             blinky.chasePlayer(currentPos)
             inky.chasePlayer(currentPos, kys, blinky.getPosition())
             pinky.chasePlayer(currentPos, kys)
-        blinky.Render(screen, dt)
-        inky.Render(screen, dt)
-        pinky.Render(screen, dt)
-        clyde.Render(screen, dt)
+        blinky.render(screen, dt)
+        inky.render(screen, dt)
+        pinky.render(screen, dt)
+        clyde.render(screen, dt)
 
-        heroBox = hero.getBoundBox()
-        # if game.collidesWithWall(heroBox):
-        # hero.setPosition(currentPos)
-        # hero.setDirection(0)
-        pelletCheck = game.collidesWithPellet(heroBox)
+        pacmanBox = pacman.getBoundBox()
+        # if game.collidesWithWall(pacmanBox):
+        # pacman.setPosition(currentPos)
+        # pacman.setDirection(0)
+        pelletCheck = board.collidesWithPellet(pacmanBox)
         if pelletCheck == 1:
             game.addScore(10)
         elif pelletCheck == 2:
             game.addScore(50)
-            blinky.scareGhost()
-            inky.scareGhost()
-            pinky.scareGhost()
-            clyde.scareGhost()
+            ghosts.scareGhosts()
 
-        if ghostCollision():
+        if ghosts.ghostCollision(pacmanBox, screen):
             game.addLives(-1)
-            hero.Restart()
-            blinky = Blinky()
-            inky = Inky()
-            pinky = Pinky()
+            pacman.restart()
+            ghosts.resetGhosts()
 
         if game.getLives() == 0:
             print("Ran out of lives!")
@@ -500,10 +566,10 @@ def main():
 
     leaderboard = database.Leaderboard()
 
-    userName = input("Enter Name: ")
+    # userName = input("Enter Name: ")
 
-    currentTime = datetime.datetime.now()
-    leaderboard.InputScore(userName, str(currentTime.strftime("%x")), game.getTime(), game.getScore())
+    # currentTime = datetime.datetime.now()
+    # leaderboard.InputScore(userName, str(currentTime.strftime("%x")), game.getTime(), game.getScore())
     leaderboard.Close()
 
 
