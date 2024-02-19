55e8d7633dbe4c38f38bd184fb6861712ffa50f3
Tomsondon <125699372+Tomsondon@users.noreply.github.com>
2024-02-19 09:35:46 +0000

    Update structures.py

--- structures.py
+++ structures.py
@@ -1,4 +1,4 @@
-class EventStack:
+class Stack:
     def __init__(self, maxSize):
         self.stack = []
         self.maxSize = maxSize
@@ -6,7 +6,7 @@ class EventStack:
     def push(self, value):
         self.stack.append(value)
         if self.isFull():
-            self.stack = self.stack[1:]
+            raise Exception("Stack is full")
 
     def pop(self):
         if not (self.isEmpty()):
@@ -15,6 +15,9 @@ class EventStack:
             return poppedItem
         else:
             raise Exception("Stack is empty, cannot pop")
+
+    def empty(self):
+        self.stack = []
 
     def peek(self):
         if not (self.isEmpty()):
@@ -31,4 +34,40 @@ class EventStack:
             return True
 
 
+class Queue:
+    def __init__(self, maxSize):
+        self.queue = []
+        self.maxSize = maxSize
+
+    def enqueue(self, item):
+        if not self.isFull():
+            self.queue.append(item)
+        else:
+            raise Exception("Queue is full")
+
+    def dequeue(self):
+        if not self.isEmpty():
+            removedItem = self.queue[0]
+            self.queue = self.queue[1:]
+            return removedItem
+        else:
+            raise Exception("Queue is empty, cannot remove item")
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.queue[0]
+        else:
+            raise Exception("Queue is empty, cannot peek")
+
+    def isEmpty(self):
+        if len(self.queue) == 0:
+            return True
+        else:
+            return False
+
+    def isFull(self):
+        if len(self.queue) == self.maxSize:
+            return True
+        else:
+            return False
 
