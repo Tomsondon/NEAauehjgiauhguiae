223bb8753259b040894bfab7bee125be9eb7c746
Tomsondon <125699372+Tomsondon@users.noreply.github.com>
2024-02-19 10:03:10 +0000

    Update structures.py
    Final

--- structures.py
+++ structures.py
@@ -1,73 +1,72 @@
 class Stack:
     def __init__(self, maxSize):
-        self.stack = []
-        self.maxSize = maxSize
+        self.__stack = []
+        self.__maxSize = maxSize
 
     def push(self, value):
-        self.stack.append(value)
+        self.__stack.append(value)
         if self.isFull():
             raise Exception("Stack is full")
 
     def pop(self):
         if not (self.isEmpty()):
-            poppedItem = self.stack[-1]
-            self.stack = self.stack[:-1]
+            poppedItem = self.__stack[-1]
+            self.__stack = self.__stack[:-1]
             return poppedItem
         else:
             raise Exception("Stack is empty, cannot pop")
 
     def empty(self):
-        self.stack = []
+        self.__stack = []
 
     def peek(self):
         if not (self.isEmpty()):
-            return self.stack[-1]
+            return self.__stack[-1]
         else:
             raise Exception("Stack is empty, cannot peek")
 
     def isEmpty(self):
-        if len(self.stack) == 0:
+        if len(self.__stack) == 0:
             return True
 
     def isFull(self):
-        if len(self.stack) == self.maxSize:
+        if len(self.__stack) == self.__maxSize:
             return True
 
 
 class Queue:
     def __init__(self, maxSize):
-        self.queue = []
-        self.maxSize = maxSize
+        self.__queue = []
+        self.__maxSize = maxSize
 
     def enqueue(self, item):
         if not self.isFull():
-            self.queue.append(item)
+            self.__queue.append(item)
         else:
             raise Exception("Queue is full")
 
     def dequeue(self):
         if not self.isEmpty():
-            removedItem = self.queue[0]
-            self.queue = self.queue[1:]
+            removedItem = self.__queue[0]
+            self.__queue = self.__queue[1:]
             return removedItem
         else:
             raise Exception("Queue is empty, cannot remove item")
 
     def peek(self):
         if not self.isEmpty():
-            return self.queue[0]
+            return self.__queue[0]
         else:
             raise Exception("Queue is empty, cannot peek")
 
     def isEmpty(self):
-        if len(self.queue) == 0:
+        if len(self.__queue) == 0:
             return True
         else:
             return False
 
     def isFull(self):
-        if len(self.queue) == self.maxSize:
+        if len(self.__queue) == self.__maxSize:
             return True
         else:
             return False
-
